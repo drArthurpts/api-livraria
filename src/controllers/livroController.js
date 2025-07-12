d06c2105831fdb5fe3ddddcd18e7f7689c318b3b
drArthurpts <arthurpatriciosousa@gmail.com>
2025-07-12 14:17:15 -0300

    Controller finalizado

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,17 +2,59 @@ import { json } from 'express';
 import livro from '../models/livro.js';
 
 class LivroController {
+    static async cadastrarLivro(req, res) {
+        try {
+            const novoLivro = new livro(req.body);
+            await novoLivro.save();
+            res.status(201).json(novoLivro);
+        } catch (erro) {
+            res.status(500).json({ message: `Erro ao cadastrar livro: ${erro.message}` });
+        }
+    }
+
     static async listarLivros(req, res) {
-       const listaLivros = await livro.find({});
-       res.status(200).json(listaLivros);
+       try {
+           const listaLivros = await livro.find({});
+           res.status(200).json(listaLivros);
+       } catch (erro) {
+           res.status(500).json({ message: `Erro ao listar livros: ${erro.message}` });
+       }
+       
 }
 
-    static async CadastrarLivro(req, res) {
+    static async listarLivroPorId(req, res) {
+        const { id } = req.params;
         try {
-           const novoLivro = await livro.create(req.body);
-           res.status(201).json({ message: "Criado com sucesso", livro: novoLivro });
+            const livroEncontrado = await livro.findById(id);
+            if (!livroEncontrado) {
+                return res.status(404).json({ message: 'Livro não encontrado' });
+            }
+            res.status(200).json(livroEncontrado);
         } catch (erro) {
-            res.status(500).json({ message: `Erro ao cadastrar livro: ${erro.message}` });  
+            res.status(500).json({ message: `Erro ao buscar livro: ${erro.message}` });
+        }
+    }
+
+    static async atualizarLivro(req, res) {
+        try{
+            const { id } = req.params.id; 
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: 'Livro atualizado com sucesso!' });
+        } catch (erro) {
+            res.status(500).json({ message: `Erro ao atualizar livro: ${erro.message}` });
+        }          
+    }
+
+   static async deletarLivro(req, res) {
+        const { id } = req.params;
+        try {
+            const livroDeletado = await livro.findByIdAndDelete(id);
+            if (!livroDeletado) {
+                return res.status(404).json({ message: 'Livro não encontrado' });
+            }
+            res.status(200).json({ message: 'Livro deletado com sucesso!' });
+        } catch (erro) {
+            res.status(500).json({ message: `Erro ao deletar livro: ${erro.message}` });
         }
     }
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,5 +10,5 @@ const routes = (app) => {
     app.use(express.json(), livros);
 
 }
-
+ 
 export default routes;
